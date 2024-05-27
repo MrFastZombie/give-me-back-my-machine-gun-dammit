4dca85ea89467fc226f37226f9d7bf0f737c30d5
Skylar Wolf <MrFastZombie@users.noreply.github.com>
2024-05-26 21:30:27 -0500

    Fixed a minor LUA skill issue on my part. Should work the same in practice.

--- prototypes/entity/entity-update-externals.lua
+++ prototypes/entity/entity-update-externals.lua
@@ -1,7 +1,14 @@
-if not data.raw.car.tank.guns["tank-machine-gun"] and data.raw.car["vehicle-chaingunner"] then
+function contains(tab, val)
+	for index, value in ipairs(tab) do
+	  if value == val then return true end
+	end
+	return false
+end
+
+if not contains(data.raw.car.tank.guns, "tank-machine-gun") then
 	table.insert(data.raw.car.tank.guns, "tank-machine-gun")
 end
 
-if not data.raw.car.tank.guns["tank-flamethrower"] and data.raw.car["vehicle-flame-tank"] then
+if not contains(data.raw.car.tank.guns, "tank-flamethrower") then
 	table.insert(data.raw.car.tank.guns, "tank-flamethrower")
 end
