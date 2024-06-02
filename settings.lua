ea9ad0a38b16096d5e60e76e2a61445317b8784b
Skylar Wolf <MrFastZombie@users.noreply.github.com>
2024-06-01 21:14:00 -0500

    Implemented settings

---
+++ settings.lua
@@ -0,0 +1,20 @@
+data:extend({
+    {
+        type = "bool-setting",
+        name = "give-mg-dammit",
+        setting_type = "startup",
+        default_value = true
+    },
+    {
+        type = "bool-setting",
+        name = "give-flamethrower-dammit",
+        setting_type = "startup",
+        default_value = true
+    },
+    {
+        type = "bool-setting",
+        name = "undo-name-dammit",
+        setting_type = "startup",
+        default_value = false
+    }
+})
